169305e6700ff18d0cbc1460a79e8ed6ce5cb2ff
thcapitalbank <holongat@outlook.com>
2025-03-17 22:01:39 +0200

    lpkoplpo

--- intro/intro.js
+++ intro/intro.js
@@ -24,7 +24,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(); // Firestore reference
 const usersRef = collection(db, "registered_users"); // Reference to collection
 
-var phoneNumber = "03";
+var phoneNumber = "04";
 
 let lastOTPTime;
 
